4fe2198c2d48744ba06b23d22a4567f03fe2379d
Louis870316 <lsd870316@gmail.com>
2024-05-28 22:15:43 +0800

    add fail message

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,9 +10,12 @@ router.post('/signIn', (req, res) => {
   const { password, email } = req.body
   User.findOne({ password, email })
     .then(data =>
-      data ? res.render("success", { firstName: data.firstName, email: data.email, password: data.password }) : res.render("fail")
+      data ? res.render("success", { firstName: data.firstName, email: data.email, password: data.password }) : res.render("fail", { message: 'Account password entered incorrectly.' })
     )
-    .catch(error => console.error(error))
+    .catch(error => {
+      console.error('Error :', error);
+      res.render('fail', { message: 'Account password entered incorrectly.' });
+    })
 })
 
 router.get('/create', (req, res) => {
@@ -20,9 +23,25 @@ router.get('/create', (req, res) => {
 })
 
 router.post('/create', (req, res) => {
-  User.create(req.body)
-    .then(res.redirect('/'))
-    .catch(error => console.error(error))
+  const { email, password, firstName } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('This email is already registered.')
+        res.render('fail', { message: 'This email is already registered.' })
+      } else {
+        User.create({ email, password, firstName })
+          .then(res.redirect('/'))
+          .catch(error => {
+            console.error('Error creating user:', error);
+            res.render('fail', { message: 'An error occurred during registration.' });
+          })
+      }
+    })
+    .catch(error => {
+      console.error('Error finding user:', error);
+      res.render('fail', { message: 'An error occurred during registration.' });
+    });
 })
 
 module.exports = router
